8d4f21b56549e2ee27d4bbf2225bd65b2cd8509e
SANG LE <sqle@ualberta.ca>
2021-10-18 17:25:02 -0600

    fix minor bug

--- polls/views.py
+++ polls/views.py
@@ -39,5 +39,5 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('results', args=(question.id,))) # 'polls:results'
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # 'polls:results'
 
